43fa2a08cc237b1cd2929bfa51082dc36a678316
henewi <hpdunn2002@gmail.com>
2022-11-07 16:30:51 +0000

    test1

--- javascript.js
+++ javascript.js
@@ -4,3 +4,16 @@
 //     myLibrary.push(document.getElementById("userinput").value);
 //     document.getElementById("data").innerHTML = myLibrary;
 // }
+
+function insert() {
+    title.push(titleInput.value);
+    author.push(authorInput.value);
+    pages.push(pagesInput.value);
+}
+
+var titleInput = []
+
+function newBook(title) {
+    titleInput.push(title);
+    console.log("bookInfo");
+}
