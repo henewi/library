fe866f47f4dca6c08603f7673fe9e8da5f199349
henewi <hpdunn2002@gmail.com>
2022-11-07 11:47:27 +0000

    testing js option 3

--- javascript.js
+++ javascript.js
@@ -9,9 +9,4 @@ function addBookToLibrary() {
     console.log(myLibrary)
 }
 
-var data = document.getElementById("data");
-myLibrary.forEach(function(element) {
-    var userinput = document.create('div');
-    userinput.innerHTML = element.userinout;
-    data.appendChild(userinput);
-})
+document.getElementById("data").innerHTML = myLibrary;
