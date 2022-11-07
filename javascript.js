f74b9d6594768ea0f46c72928bad8c898ce0ecd7
henewi <hpdunn2002@gmail.com>
2022-11-07 12:25:16 +0000

    finished

--- javascript.js
+++ javascript.js
@@ -2,17 +2,7 @@ var myLibrary = [];
 
 function addBookToLibrary() {
     myLibrary.push(document.getElementById("userinput").value);
-    console.log(myLibrary)
+    document.getElementById("data").innerHTML = myLibrary;
 }
-
-
-function Book() {
-    var page='';
-    for (var i=0; i<Book.length; i++) {
-        page+='<br>'+myLibrary[i]+'</div>';
-    }
-    return html;
-}
-document.getElementById('data').innerHTML+= html;
 
 // document.getElementById("data").innerHTML = myLibrary;
