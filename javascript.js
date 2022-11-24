d4a81c70718196b1abe6f760c5c06a0c3f9f2b8d
henewi <hpdunn2002@gmail.com>
2022-11-24 19:26:23 +0000

    rm all useless //

--- javascript.js
+++ javascript.js
@@ -4,42 +4,6 @@
 // 4. Display each section of the library on the page
 // 5. Allow the library to be recalled at any time
 
-//-----------------------------------------------------------------
-
-// var myLibrary = [];
-
-// function addBookToLibrary() {
-//     myLibrary.push(document.getElementById("userinput").value);
-//     document.getElementById("data").innerHTML = myLibrary;
-// }
-
-// function insert() {
-//     title.push(titleInput.value);
-//     author.push(authorInput.value);
-//     pages.push(pagesInput.value);
-// }
-
-// var titleInput = []
-
-// function newBook(title) {
-//     titleInput.push(title);
-//     console.log("bookInfo");
-// }
-
-//-----------------------------------------------------------------
-
-// const details = {
-//     firstname: 'Henry',
-//     lastname: 'Dunn',
-//     age: '19',
-//     "favnum": function() {
-//         console.log(99-30);
-//     }
-// }
-
-// console.log(details.firstname);
-// console.log(details["favnum"]);
-
 // .projectscont
 
 const books = document.querySelector('projectscont')
