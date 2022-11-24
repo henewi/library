41abf99a87efb6ca1e4e52a65a4862b78b0d105d
henewi <hpdunn2002@gmail.com>
2022-11-24 19:20:12 +0000

    book creator

--- javascript.js
+++ javascript.js
@@ -48,18 +48,27 @@ const myLibrary = [{
     author: 'me',
     pages: 500,
     read: true,
-}{
+},
+{
     title: 'Book2',
     author: 'you',
     pages: 5000,
     read: false,
-}]
+},];
+
+function createBookElement(el,content,className) {
+    const element = document.createElement(el);
+    element.textcontent = content;
+    element.setAttribute("class",className);
+    return element;
+}
 
 function createBookItem (book,index) {
     const bookItem = document.createElement('div');
     bookItem.setAttribute('id',index)
     bookItem.setAttribute('key',index)
     bookItem.setAttribute('class', 'card book')
+    bookItem.appendChild(createBookElement('h1',"Title:" + book.title, 'book-title'))
     
 }
 
