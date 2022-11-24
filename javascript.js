d33cc2ebbb75a7aa4ee80c7a44f6dcce6f3cbce1
henewi <hpdunn2002@gmail.com>
2022-11-24 19:04:26 +0000

    book array filled

--- javascript.js
+++ javascript.js
@@ -43,16 +43,28 @@
 // .projectscont
 
 const books = document.querySelector('projectscont')
-const myLibrary = []
+const myLibrary = [{
+    title: 'Book1',
+    author: 'me',
+    pages: 500,
+    read: true,
+}{
+    title: 'Book2',
+    author: 'you',
+    pages: 5000,
+    read: false,
+}]
 
 function createBookItem (book,index) {
     const bookItem = document.createElement('div');
     bookItem.setAttribute('id',index)
     bookItem.setAttribute('key',index)
+    bookItem.setAttribute('class', 'card book')
+    
 }
 
 function renderBooks () {
     myLibrary.map((book,index)) => {
-        createBookItem(book,index)
+        createBookItem(book,index);
     }
 }
